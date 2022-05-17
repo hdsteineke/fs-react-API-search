22693a341a7e81b59c717f0feda07bf4049df41a
HAILEY STEINEKE <hailey.steineke@gmail.com>
2022-05-16 17:14:28 -0700

    added loading spinner

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import './App.css';
 import { useEffect, useState } from 'react';
 import { getPokemon } from './services/fetch-utils';
+import Spinner from './Spinner';
 
 function App() {
   const [pokemon, setPokemon] = useState([]);
   const [query, setQuery] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
 
   async function load() {
+    setIsLoading(true);
+    
     const { data: { results }, } = await getPokemon(query);
     setPokemon(results);
+    
+    setIsLoading(false);
   }
   
+
   useEffect(() => {
     load();
 
@@ -18,7 +25,7 @@ function App() {
 
   async function handleSearch(e) {
     e.preventDefault();
-    
+
     load();
     
   }
@@ -30,16 +37,17 @@ function App() {
         <button>Search</button>
       </form>
       <header className="App-header">
-        {pokemon.map(({ pokebase, url_image, hp, type_1, type_2, ability_1 }, i) => 
-          <div key={pokebase + i}>
-            <h3>{pokebase}</h3>
-            <img src={url_image} />
-            <p>hp: {hp}</p>
-            <p>type 1: {type_1}</p>
-            <p>type 2: {type_2}</p>
-            <p>special ability: {ability_1}</p>
-          </div>
-        )}
+        { isLoading ? <Spinner /> : 
+          pokemon.map(({ pokebase, url_image, hp, type_1, type_2, ability_1 }, i) => 
+            <div key={pokebase + i}>
+              <h3>{pokebase}</h3>
+              <img src={url_image} />
+              <p>hp: {hp}</p>
+              <p>type 1: {type_1}</p>
+              <p>type 2: {type_2}</p>
+              <p>special ability: {ability_1}</p>
+            </div>
+          )}
       </header>
     </div>
   );
